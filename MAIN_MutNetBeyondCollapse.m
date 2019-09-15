4c2ddfe4aea3ee6ee5f13df737ca21dbe00b51a3
jelle <>
2019-09-15 12:30:52 +0100

    New way to decide whether files need to be replaced (i.e. all steps at once)

--- MAIN_MutNetBeyondCollapse.m
+++ MAIN_MutNetBeyondCollapse.m
@@ -12,62 +12,60 @@ close all
 %% randomize
 rng('shuffle');
 
+%% replace files (make new simulation and analysis)
+replaceFiles = true;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Step 1: Make time series (using GRIND for MATLAB) %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% load time series (false) or make new simulation (true)
-replaceCHintMUTnumericalFile = true;
-CHintMUTnumericalFile = code.gradual_change.CHintMUTnumerical(NETnr,SETnr,replaceCHintMUTnumericalFile);
+CHintMUTnumericalFile = code.gradual_change.CHintMUTnumerical(NETnr,SETnr,replaceFiles);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Step 2: Get Mean and CI - for timeseriesplot %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Settings
-replaceAnalysemeanRangeFile = true;
 meanRange_window=50;
 meanRange_stepsize=50;
 
 %% Analysis
-analyseMeanRangeFile = code.timeseries_analysis.meanRange(NETnr,SETnr,meanRange_window,meanRange_stepsize,CHintMUTnumericalFile,replaceAnalysemeanRangeFile);
+analyseMeanRangeFile = code.timeseries_analysis.meanRange(NETnr,SETnr,meanRange_window,meanRange_stepsize,CHintMUTnumericalFile,replaceFiles);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Step 3: Analyze Tipping Point %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Settings
-replaceAnalyseTppFile = true;
 analyseTPP_window_dAb=200;
 analyseTPP_window_PCA=2000;
 analyseTPP_stepsize=200;
 
 %% Analysis
-analyseTppFile = code.timeseries_analysis.analyseTPP(NETnr,SETnr,analyseTPP_window_dAb,analyseTPP_window_PCA,analyseTPP_stepsize,CHintMUTnumericalFile,replaceAnalyseTppFile);
+analyseTppFile = code.timeseries_analysis.analyseTPP(NETnr,SETnr,analyseTPP_window_dAb,analyseTPP_window_PCA,analyseTPP_stepsize,CHintMUTnumericalFile,replaceFiles);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Step 4: Analyze Time Series %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Settings
-replaceAnalyseMakePCAskewFile = true;
 makePCAskew_window=2000;
 makePCAskew_stepsize=200;
 
 %% Analysis
-analyseMakePCAskewFile = code.timeseries_analysis.makePCA(NETnr,SETnr,makePCAskew_window,makePCAskew_stepsize,CHintMUTnumericalFile,analyseTppFile,replaceAnalyseMakePCAskewFile);
+analyseMakePCAskewFile = code.timeseries_analysis.makePCA(NETnr,SETnr,makePCAskew_window,makePCAskew_stepsize,CHintMUTnumericalFile,analyseTppFile,replaceFiles);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Step 5: Determine Critical Range - trend in Expl. Var. %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Settings Analysis 4: Critical range
-replaceAnalyseCriticalRangeFile = true;
 ExplVartrend_window=10;
 ExplVartrend_stepsize=1;
 
 %% Analysis 4: determine and analyse critical range
-analyseCriticalRangeFile = code.timeseries_analysis.analyseCriticalRange(NETnr,SETnr,ExplVartrend_window,ExplVartrend_stepsize,analyseTppFile,analyseMakePCAskewFile,replaceAnalyseCriticalRangeFile);
+analyseCriticalRangeFile = code.timeseries_analysis.analyseCriticalRange(NETnr,SETnr,ExplVartrend_window,ExplVartrend_stepsize,analyseTppFile,analyseMakePCAskewFile,replaceFiles);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Step 6: plot figures %%%%
